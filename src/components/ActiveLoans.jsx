ce56d49cf4331cdc372da6c92450c34d1adac900
Kaishiyoku <dmp@nfze.de>
2021-03-08 21:26:14 +0100

    adjusted dashboard layout

--- src/components/ActiveLoans.jsx
+++ src/components/ActiveLoans.jsx
@@ -4,6 +4,7 @@ import {format, parseISO} from 'date-fns';
 import Button from './button/Button';
 import clsx from 'clsx';
 import LabelWithValueGroup from './LabelWithValueGroup';
+import {length} from 'ramda';
 
 function ActiveLoans(props) {
     const getLoanDisplayValuesFor = (loan) => [
@@ -13,25 +14,25 @@ function ActiveLoans(props) {
         {label: 'Status', value: loan.status},
     ];
 
-    const renderLoans = props.loans.map((loan) => (
+    const renderLoans = length(props.loans) > 0 ? props.loans.map((loan) => (
         <div key={loan.id}>
             <LabelWithValueGroup entries={getLoanDisplayValuesFor(loan)}/>
 
             <Button label="Pay back"/>
         </div>
-    ));
+    )) : <div className="text-gray-500 italic">You don't have any active loans.</div>;
 
     return (
-        <div className={clsx('rounded-lg overflow-hidden shadow-lg border border-gray-100 bg-white', props.className)}>
-            <div className="font-bold text-xl px-6 py-4">Active loans</div>
-            <div className="px-6 pb-4">
+        <div className={props.className}>
+            <div className="text-xl pb-4">Active loans</div>
+
+            <div className="grid md:grid-cols-2 gap-4 mb-4">
                 {renderLoans}
             </div>
-            <div className="border-t border-gray-200 bg-gray-50 px-6 py-4">
-                <Link to="/loans/available" className="text-left text-blue-600 shadow-md border border-blue-300 bg-white rounded-full transition-all duration-200 focus:ring-4 focus:ring-blue-200 hover:bg-blue-500 hover:text-white focus:outline-none px-4 py-2">
-                    Show available loans
-                </Link>
-            </div>
+
+            <Link to="/loans/available" className="text-left text-blue-600 shadow-md border border-blue-300 bg-white rounded-full transition-all duration-200 focus:ring-4 focus:ring-blue-200 hover:bg-blue-500 hover:text-white focus:outline-none px-4 py-2">
+                Show available loans
+            </Link>
         </div>
     );
 }
--- src/components/OwnedShips.jsx
+++ src/components/OwnedShips.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import {Link} from '@reach/router';
 import LabelWithValueGroup from './LabelWithValueGroup';
+import {length} from 'ramda';
 
 function OwnedShips(props) {
     const getShipDisplayValuesFor = (ship) => [
@@ -16,27 +17,27 @@ function OwnedShips(props) {
         {label: 'Weapons', value: ship.weapons},
     ];
 
-    const renderShips = props.ships.map((ship) => (
+    const renderShips = length(props.ships) > 0 ? props.ships.map((ship) => (
         <div key={ship.id} className="rounded-lg overflow-hidden shadow-lg border border-gray-100 bg-white">
             <div className="font-bold text-xl px-6 py-4">{ship.type} :: {ship.location}</div>
             <div className="px-6 pb-4">
                 <LabelWithValueGroup entries={getShipDisplayValuesFor(ship)}/>
             </div>
         </div>
-    ));
+    )) : <div className="text-gray-500 italic">You don't have any ships in your possession yet.</div>;
 
     return (
-        <>
+        <div className={props.className}>
             <div className="text-xl pb-4">Owned Ships</div>
 
-            <div className="grid md:grid-cols-2 gap-4 mb-8">
+            <div className="grid md:grid-cols-2 gap-4 mb-4">
                 {renderShips}
             </div>
 
             <Link to="/ships/available" className="text-left text-blue-600 shadow-md border border-blue-300 bg-white rounded-full transition-all duration-200 focus:ring-4 focus:ring-blue-200 hover:bg-blue-500 hover:text-white focus:outline-none px-4 py-2">
                 Show available ships
             </Link>
-        </>
+        </div>
     );
 }
 
